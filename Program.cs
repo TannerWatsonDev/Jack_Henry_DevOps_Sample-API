043d21123f45bc238273728951fc9b86cef5ad33
Tanner Watson <tanner.watsondev@gmail.com>
2026-02-20 12:42:11 -0600

    Add Dockerfile to containerize API

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-    
-// Initialize builder variable that sets up config, services, logging, and hosting for web app
+
+// prepares configuration/services/logging and defaults
 var builder = WebApplication.CreateBuilder(args);
 // Builds the web application using the configured services and settings
 var app = builder.Build();
 
 //Defines/Maps an HTTP GET request to the / endpoint and returns a string when that endpoint is requested.
-app.MapGet("/", () => "Jack Henry Devops Sample API");
+app.MapGet("/", () => "Jack Henry DevOps Sample API");
 //Defines/Maps an HTTP GET request to the /health endpoint and returns a string then return healthy string whenr requested.
 app.MapGet("/health", () => Results.Ok("Healthy"));
 
